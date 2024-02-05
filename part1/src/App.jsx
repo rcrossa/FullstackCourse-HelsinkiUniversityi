81570b96daeddba7bcdb748a3bef46417cc10214
Roberto Rossa <rcrossa@hotmail.com>
2024-02-05 13:04:58 -0300

    Step 2 done.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,21 @@
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
+  const Part = (props) => {
+    return (
+      <>
+        <p>
+          {props.part}
+        </p>
+      </>
+    )
+  }
   const Header = (props) => {
     return (
       <>
@@ -10,11 +26,11 @@ const App = () => {
 
   const Content = () => {
     return (
-      <>
-        <p>
-          Content
-        </p>
-      </>
+      <div>
+        <Part />
+        <Part />
+        <Part />
+      </div>
     )
   }
 
@@ -25,7 +41,6 @@ const App = () => {
       </p></>
     )
   }
-  const course = 'Half Stack application development'
   return (
     <div>
       <Header course={course} />
